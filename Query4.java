3c8d1bebeea8a83dc61f44292da055246203a2a0
dlsuWang <kennethwang17@yahoo.com>
2016-02-29 19:21:34 +0800

    added index

--- Query4.java
+++ Query4.java
@@ -14,8 +14,9 @@ import java.util.*;
  */
 public class Query4{
     
-    private String normalQuery, heuristicsQuery, indexQuery, storedProcedureQuery;
-    private ArrayList<String> execQuery = new ArrayList<String>();
+    private String normalQuery, heuristicsQuery, indexSql, storedProcedureQuery;
+    private ArrayList<String> viewQuery = new ArrayList<String>();
+    private ArrayList<String> indexQuery = new ArrayList<String>();
     private ArrayList<String> spQuery = new ArrayList<String>();
     
     private String append;
@@ -24,6 +25,7 @@ public class Query4{
         NormalQuery(ae1,ae2,ae3,ae4,ae5,ae6,ae7,ae8,ae9,ae10,ae11,ae12,ae13,ae14,ae15,ae16,ae17,ae18,sugarcane,palay,corn,others);
         ViewsQuery(ae1,ae2,ae3,ae4,ae5,ae6,ae7,ae8,ae9,ae10,ae11,ae12,ae13,ae14,ae15,ae16,ae17,ae18,sugarcane,palay,corn,others);
         StoredProcedureQuery(ae1,ae2,ae3,ae4,ae5,ae6,ae7,ae8,ae9,ae10,ae11,ae12,ae13,ae14,ae15,ae16,ae17,ae18,sugarcane,palay,corn,others);
+        IndexQuery(ae1,ae2,ae3,ae4,ae5,ae6,ae7,ae8,ae9,ae10,ae11,ae12,ae13,ae14,ae15,ae16,ae17,ae18,sugarcane,palay,corn,others);
     }
     
     public void NormalQuery(int ae1, int ae2, int ae3, int ae4, int ae5, int ae6, int ae7, int ae8, int ae9, int ae10, int ae11, int ae12, int ae13, int ae14, int ae15, int ae16, int ae17, int ae18, int sugarcane, int palay, int corn, int others){
@@ -45,7 +47,153 @@ public class Query4{
     }
 
     public void IndexQuery(int ae1, int ae2, int ae3, int ae4, int ae5, int ae6, int ae7, int ae8, int ae9, int ae10, int ae11, int ae12, int ae13, int ae14, int ae15, int ae16, int ae17, int ae18, int sugarcane, int palay, int corn, int others) {
+    
+        ArrayList list = new ArrayList<Integer>();
+        list.add(ae1);
+        list.add(ae2);
+        list.add(ae3);
+        list.add(ae4);
+        list.add(ae5);
+        list.add(ae6);
+        list.add(ae7);
+        list.add(ae8);
+        list.add(ae9);
+        list.add(ae10);
+        list.add(ae11);
+        list.add(ae12);
+        list.add(ae13);
+        list.add(ae14);
+        list.add(ae15);
+        list.add(ae16);
+        list.add(ae17);
+        list.add(ae18);
+    
+        boolean useIndex = false;
+        
+        for(int a=0; a < list.size(); a++)
+        {
+            if((int)list.get(a) == 1)
+            {
+                useIndex = true;
+                break;
+            }
+        }
+
+        if(useIndex)
+        {
+            indexSql = "Create Index indexOne on hpq_hh(id, ";
+            
+            boolean isFirst = true;
+            
+            for(int a=0; a < list.size(); a++)
+            {
+                if((int)list.get(a) == 1 && isFirst)
+                {
+                    isFirst = false;
+                    
+                    indexSql = indexFunction(indexSql, a+1);
+                }
+                else if((int)list.get(a) == 1 && !isFirst)
+                {
+                    indexSql = indexSql + ", ";
+                    indexSql = indexFunction(indexSql, a+1);
+                }
+            }
+            
+             indexSql = indexSql + ");"; 
+        }
+        else
+        {
+            indexSql = "Create Index indexOne on hpq_hh(id);";
+        }
+        
+        indexQuery.add(indexSql);
+        indexQuery.add("Create Index indexTwo on hpq_crop(croptype);");
+        indexQuery.add(normalQuery + ";");
+        indexQuery.add("Alter Table hpq_hh Drop Index indexOne;");
+        indexQuery.add("Alter Table hpq_crop Drop Index indexTwo;");
+
     }
+    
+     public String indexFunction(String indexSql, int column){
+     
+        switch(column){
+            case 1:
+                indexSql = indexSql + "agriequip1_nown";
+            break;
+            
+            case 2:
+                indexSql = indexSql + "agriequip2_nown";
+            break;
+            
+            case 3:
+                indexSql = indexSql + "agriequip3_nown";
+            break;
+            
+            case 4:
+                indexSql = indexSql + "agriequip4_nown";
+            break;
+            
+            case 5:
+                indexSql = indexSql + "agriequip5_nown";
+            break;
+
+            case 6:
+                indexSql = indexSql + "agriequip6_nown";
+            break;
+            
+            case 7:
+                indexSql = indexSql + "agriequip7_nown";
+            break;
+            
+            case 8:
+                indexSql = indexSql + "agriequip8_nown";
+            break;
+            
+            case 9:
+                indexSql = indexSql + "agriequip9_nown";
+            break;
+            
+            case 10:
+                indexSql = indexSql + "agriequip10_nown";
+            break;
+            
+            case 11:
+                indexSql = indexSql + "agriequip11_nown";
+             break;
+             
+            case 12:
+                indexSql = indexSql + "agriequip12_nown";
+             break;
+            
+            case 13:
+                indexSql = indexSql + "agriequip13_nown";
+             break;
+            
+            case 14:
+                indexSql = indexSql + "agriequip14_nown";
+             break;
+            
+            case 15:
+                indexSql = indexSql + "agriequip15_nown";
+             break;
+            
+            case 16:
+                indexSql = indexSql + "agriequip16_nown";
+             break;
+            
+            case 17:
+                indexSql = indexSql + "agriequip17_nown";
+             break;
+            
+            case 18:
+                indexSql = indexSql + "agriequip18_nown";
+                
+        }
+                 
+        return indexSql;
+    }
+    
 
     public void ViewsQuery(int ae1, int ae2, int ae3, int ae4, int ae5, int ae6, int ae7, int ae8, int ae9, int ae10, int ae11, int ae12, int ae13, int ae14, int ae15, int ae16, int ae17, int ae18, int sugarcane, int palay, int corn, int others) {
         String create, create2 ,query, drop1, drop2;
@@ -73,11 +221,11 @@ public class Query4{
         drop1 = "DROP VIEW crop_view;\n";
         drop2 = "DROP VIEW id_view;";
         
-        execQuery.add(create);
-        execQuery.add(create2);
-        execQuery.add(query);
-        execQuery.add(drop1);
-        execQuery.add(drop2);
+        viewQuery.add(create);
+        viewQuery.add(create2);
+        viewQuery.add(query);
+        viewQuery.add(drop1);
+        viewQuery.add(drop2);
         
     }
 
@@ -93,12 +241,11 @@ public class Query4{
         spQuery.add("CALL getSPQuery4();");
     }
 
+    
+    
+    
     public String getNormalQuery() {
         return normalQuery;
-    }
-
-    public String getIndexQuery() {
-        return indexQuery;
     }
 
     public String getHeuristicsQuery() {
@@ -109,8 +256,20 @@ public class Query4{
         return spQuery;
     }
 
-    public ArrayList<String> getQueryWithCreateDropFunction() {
-        return execQuery;
+    public ArrayList<String> getQueryWithCreateDropFunction(String keyword) {
+        
+        if(keyword.equals("view"))
+        {
+            return viewQuery;
+        }
+        else
+        {
+            return indexQuery;
+        }
+        
     }
 
+    
+    
+    
 }
